ee719f2a34d66f2b6738a7cc5ab2d425e7b76a6c
Aritraz <aritrasarkaraec@gmail.com>
2023-06-24 13:13:58 +0530

    Added SellerHomePage

---
+++ reactapp/src/pages/seller/SellerHomepage.js
@@ -0,0 +1,19 @@
+import DashBoard from "../../components/seller/Dashboard";
+import ProductHeader from "../../components/seller/ProductHeader";
+import SellerNavigationBar from "../../components/seller/SellerNavigationBar";
+import Footer from "../../components/common/Footer";
+import AllProducts from "../../components/seller/AllProducts";
+
+
+export default function SellerHomePage() {
+    return (
+        <div>
+            <SellerNavigationBar/>
+            <DashBoard />
+            <br/>
+            <ProductHeader />
+            <AllProducts/>
+            <Footer/>
+        </div>
+    );
+}
