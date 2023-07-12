f54efd77a4aaf2e28e4bf8b509c866576ea01ea6
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2023-07-13 02:28:42 +0530

    Updated Feature

--- reactapp/src/pages/buyer/PurchaseHistory.jsx
+++ reactapp/src/pages/buyer/PurchaseHistory.jsx
@@ -6,16 +6,21 @@ import PurchaseTable from '../../components/buyer/PurchaseTable';
 import NavigationBar from '../../components/common/NavigationBar';
 import { useSelector } from "react-redux";
 import Footer from '../../components/common/Footer';
-
-
+import { useNavigate } from "react-router-dom";
+import {MdKeyboardBackspace} from 'react-icons/md';
 
 const PurchaseHistory = () => {
 
     const [data, setData] = useState([]);
     const [load, setLoad] = useState(false);
+    const navigate = useNavigate();
     // const buyerId = 2;
     const buyerId = useSelector((state) => state.user.currentUser.id);
     console.log(buyerId)
+
+    const handleGoBack = () => {
+        navigate("/home")
+    };
 
     const getBuyerData = async () => {
         try {
@@ -39,13 +44,11 @@ const PurchaseHistory = () => {
             <NavigationBar />
             <br />
             <br />
-            <Card>
-                <Card.Body>
-                    <Card.Title>
-                        <h3>Purchase History</h3>
-                    </Card.Title>
-                </Card.Body>
-            </Card>
+            <br/>
+            <div className="d-flex flex-row align-items-center">
+                <p className='ms-3' ><MdKeyboardBackspace style={{color:"grey"}} onClick={handleGoBack}/>{" "}<a href="#" style={{color:"grey"}} onClick={handleGoBack}>Back</a></p>
+                <p className='ms-3' style={{fontSize:30}}><b>PURCHASE HISTORY</b></p>
+            </div>
 
             <div className='d-flex justify-content-center p-5 '>
                 {!load ? (
