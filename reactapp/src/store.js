19ab79a848127b56a7a6380bdeff80da1008e459
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2023-07-12 18:38:10 +0530

    Updated login

--- reactapp/src/store.js
+++ reactapp/src/store.js
@@ -1,14 +1,25 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers,configureStore} from "@reduxjs/toolkit";
 import {userReducer} from './features/userSlice';
 import { productReducer } from "./features/productSlice";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const rootReducer = combineReducers({
+    user:userReducer,
+    product:productReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer: {
-        user:userReducer,
-        product:productReducer
+    reducer: persistedReducer,
+});
 
-    }
-    }
-)
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
