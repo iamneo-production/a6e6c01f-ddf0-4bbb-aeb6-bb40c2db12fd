54cfefe6cae277d7ae8f2633658206635709e554
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2023-07-16 00:12:32 +0530

    Integrated seller homepage

--- reactapp/src/components/seller/AllProducts.js
+++ reactapp/src/components/seller/AllProducts.js
@@ -1,5 +1,7 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import { getSellerProducts } from '../../features/productSlice';
 
 const products = [
     {
@@ -18,23 +20,32 @@ const products = [
 ];
 
 const AllProducts = () => {
+
+    const navigate = useNavigate();
+    const token = useSelector(state => state.user.token)
+    const dispatch = useDispatch()
+    useEffect(() =>{
+        dispatch(getSellerProducts({token:token}))
+    },[])
+    const sellerProductsList = useSelector(state => state.product.sellerProductsList);
+
     return (
         <div>
             {
-                products.map((prod, index) =>
+                sellerProductsList.map((prod, index) =>
                 (
                     <div>
                         <div style={{ display: 'flex', justifyContent: 'center' }} key={index}>
                             <div className="row">
                                 <div className="col-md-2" style={{ marginLeft: 40 }}>
-                                    <img src={prod.image} alt="Card" className="d-block " width={155} height={135} />
+                                    <img src={`data:image/jpeg;base64,${prod.image}`} alt="Card" className="d-block " width={155} height={135} />
                                     <div className='prod-price d-inline mt-4 mb-2 ms-4 fw-bold '>{prod.quantity}</div>
                                 </div>
 
                                 <div className="col-md-9 border">
                                     <div className="card-body">
                                         <h6 className="card-title fw-bold">{prod.name}</h6>
-                                        <p className="card-text text-muted fw-bold ">{prod.cost}</p>
+                                        <p className="card-text text-muted fw-bold ">{`₹ ${prod.price.toLocaleString("en-US")}`}</p>
                                         <div className=' btn btn-light border d-inline  px-2 py-2 mb-3 fw-bold fs-6'>Buyers</div>
                                         <div className=' btn btn-light border d-inline ms-3 px-2 py-2 mb-3 fw-bold fs-6'>Edit</div>
                                         <div className=' btn btn-light border d-inline float-end px-2 py-2 mb-3 bg-danger fw-bold'>Remove</div>
