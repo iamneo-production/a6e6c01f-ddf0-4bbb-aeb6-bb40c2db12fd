7188e1ef2d79fcea493bf477ff6da0b02c421c26
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2023-07-16 00:17:00 +0530

    Updated seller products

--- reactapp/src/components/seller/AllProducts.js
+++ reactapp/src/components/seller/AllProducts.js
@@ -35,17 +35,16 @@ const AllProducts = () => {
                 sellerProductsList.map((prod, index) =>
                 (
                     <div>
-                        <div style={{ display: 'flex', justifyContent: 'center' }} key={index}>
+                        <div className={"container"} key={index}>
                             <div className="row">
-                                <div className="col-md-2" style={{ marginLeft: 40 }}>
+                                <div className="col-2 d-flex flex-column align-items-center">
                                     <img src={`data:image/jpeg;base64,${prod.image}`} alt="Card" className="d-block " width={155} height={135} />
-                                    <div className='prod-price d-inline mt-4 mb-2 ms-4 fw-bold '>{prod.quantity}</div>
                                 </div>
 
-                                <div className="col-md-9 border">
+                                <div className="col-10 border">
                                     <div className="card-body">
                                         <h6 className="card-title fw-bold">{prod.name}</h6>
-                                        <p className="card-text text-muted fw-bold ">{`₹ ${prod.price.toLocaleString("en-US")}`}</p>
+                                        <p className="text-success fw-bold fs-8">{`₹ ${prod.price.toLocaleString("en-US")}`}</p>
                                         <div className=' btn btn-light border d-inline  px-2 py-2 mb-3 fw-bold fs-6'>Buyers</div>
                                         <div className=' btn btn-light border d-inline ms-3 px-2 py-2 mb-3 fw-bold fs-6'>Edit</div>
                                         <div className=' btn btn-light border d-inline float-end px-2 py-2 mb-3 bg-danger fw-bold'>Remove</div>
