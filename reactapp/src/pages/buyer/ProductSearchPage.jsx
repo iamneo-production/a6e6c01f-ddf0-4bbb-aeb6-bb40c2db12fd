487e01071efcadaf9951aec8575e6d2c9f0ffd76
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2023-07-19 12:17:27 +0530

    Added features to category and search

--- reactapp/src/pages/buyer/ProductSearchPage.jsx
+++ reactapp/src/pages/buyer/ProductSearchPage.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import "../../pages/buyer/home.css"
+import { ReactComponent as NoResult } from '../../../src/assets/NoResult.svg';
 import NavigationBar from "../../components/common/NavigationBar";
 import { useNavigate } from "react-router-dom";
 import {MdKeyboardBackspace} from 'react-icons/md';
@@ -30,12 +31,26 @@ export default function ProductSearchPage() {
             <NavigationBar/><br/><br/><br/>
             <div className='d-flex flex-row align-items-center'>
                 <p className='ms-3' ><MdKeyboardBackspace style={{color:"grey"}} onClick={handleGoBack}/>{" "}<a href="#" style={{color:"grey"}} onClick={handleGoBack}>Back</a></p>
-                <p className='ms-3' style={{fontSize:30}}><b>{searchQuery}</b></p>
+                <p className='ms-3' style={{fontSize:20}}><b>Search results for{" "}<span style={{fontSize:20,color:"dodgerblue"}}>{`"${searchQuery}"`}</span></b></p>
             </div>
             <br />
             <section className="container-xl">
                 <div className="row">
+                    {searchProductResult.length === 0 ? (
+                        <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }} >
+                            <div style={{ width: 300, height: 300 }}>
+                                <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                    <h4 style={{ color: "grey" }}><b>Oops!... no results found</b></h4>
+                                </div>
+                                <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                    <h6>Please try another search</h6>
+                                </div>
+                                <NoResult />
+                            </div>
+                        </div>
+                    ) : (
                     <Card ProductList={searchProductResult}/>
+                    )}
                 </div>
             </section>
 
